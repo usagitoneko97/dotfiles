c7dcf947f0d0ea09f29e19db0b9749b9139910c2
usagitoneko97 <hogouxian@gmail.com>
2019-10-17 21:36:20 +0800

    modify tg_status to login everytime it's called

--- scripts/.scripts/qbittorrent_scripts/tg_status.py
+++ scripts/.scripts/qbittorrent_scripts/tg_status.py
@@ -14,6 +14,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 FILTER_OPTION = {"filter": ["all", "downloading", "completed", "paused", "active", "inactive"],
                  "reverse": ["true", "false"]}
 
+VIEW_OPTION = ["ratio", "tracker", "size", "dlspeed", "uploaded"]
+
 logger = logging.getLogger(__name__)
 
 IP = "http://192.168.1.105:9001"
@@ -26,7 +28,7 @@ class Config:
         self.__dict__ = self.__state
 
     def setup(self, filter="downloading", category="", sort="", reverse="false", limit=100,
-              view_selects=None, qbit_client=None):
+              view_selects=None):
         self.torrents_filter = {
             "filter": filter,
             "category": category,
@@ -34,8 +36,13 @@ class Config:
             "reverse": reverse,
             "limit": limit,
         }
-        self.views_select = view_selects or ["ratio", "tracker", "size", "dlspeed", "uploaded"]
-        self.qbit_client = qbit_client
+        self.views_select = view_selects or VIEW_OPTION
+
+    @property
+    def qbit_client(self):
+        qb_client = qbittorrent.Client(IP)
+        qb_client.login(QBIT_LOGIN_ID, QBIT_LOGIN_PW)
+        return qb_client
 
 
 def info(update: telegram.Update, context: CallbackContext):
@@ -65,14 +72,19 @@ config_handler = tg_helper.StateHandler("config")
 @config_handler.end
 def config_view_selects_apply(update: telegram.Update, context: CallbackContext):
     config = Config()
-    config.views_select = list(update.message.text.split(" "))
+    if update.message.text == "default":
+        config.views_select = VIEW_OPTION
+    else:
+        config.views_select = list(update.message.text.split(" "))
     update.message.reply_text(f"Successfully update the views select to :{config.views_select}")
 
 
 @config_handler.message_handler(Filters.regex(r"Views"))
 @config_handler.goto(config_view_selects_apply)
 def config_view_selects_query(update: telegram.Update, context: CallbackContext):
-    update.message.reply_text("Type in the list of view to select")
+    kb = telegram.ReplyKeyboardMarkup([["default"]], one_time_keyboard=True, resize_keyboard=True)
+    update.message.reply_text("Select view from {}. Or type 'default' to restore to default".format(VIEW_OPTION),
+                              reply_markup=kb)
 
 
 @config_handler.message_handler()
@@ -132,9 +144,7 @@ class Torznab:
 if __name__ == '__main__':
     QBIT_LOGIN_ID = os.environ["QB_LOGIN_ID"]
     QBIT_LOGIN_PW = os.environ["QB_LOGIN_PW"]
-    qb_client = qbittorrent.Client(IP)
-    qb_client.login(QBIT_LOGIN_ID, QBIT_LOGIN_PW)
-    Config().setup(qbit_client=qb_client)
+    Config().setup()
     TOKEN = os.environ["TG_TOKEN"]
     updater = Updater(TOKEN, use_context=True)
     updater.dispatcher.add_handler(CommandHandler("info", info))
